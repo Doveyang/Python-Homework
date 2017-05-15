85348609ccacd525f1f43f5d22ed3611ce74767c
Nick Yang <fx6073@gmail.com>
2017-05-15 10:09:43 +0800

    Utf

--- 1601034-杨非池-005-第12讲课堂作业/Exp7_8.py
+++ 1601034-杨非池-005-第12讲课堂作业/Exp7_8.py
@@ -1,13 +1,13 @@
-#coding=UTF-8
+# coding=UTF-8
 import sys
 reload(sys)
-sys.setdefultencoding('utf-8')
+sys.setdefaultencoding('utf-8')
 f = open('F7_1.txt', 'r+')
 f.seek(6)
 f.write(u'央')
 f.seek(1)
 f.write('9')
-f.seek(0,2)
+f.seek(0, 2)
 f.write(u'人民政府')
 f.close
 
